d8d975cc801ad6c4de48e18bbceafe349cd7dee6
Tobias <stobias@sqli.com>
2019-06-05 15:54:13 +0200

    Refacto with one function

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -5,19 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './update.component.html',
   styleUrls: ['./update.component.scss']
 })
+
 export class UpdateComponent {
   public width: string = '50';
   public height: string = '50';
   public color: string = 'green';
 
-  resizeWidth(focusEvent?: Event): void {
-    if (!focusEvent) return
-    this.width = (focusEvent.target as HTMLInputElement).value
-
+  resize(width: HTMLInputElement, height: HTMLInputElement): void {
+    if (!width && !height) return;
     if (Number(this.width) > 100) this.color = 'red'
+    else this.color = 'green'
+
+    this.width = width.value;
+    this.height = height.value
   }
-  resizeHeight(focusEvent?: Event): void {
-    if (!focusEvent) return
-    this.height = (focusEvent.target as HTMLInputElement).value
+
+  reset(width: HTMLInputElement, height: HTMLInputElement): void {
+    width.value = this.width = '50'
+    height.value = this.height = '50'
+    this.color = 'green'
   }
+ 
 }
