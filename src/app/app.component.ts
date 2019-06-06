55822ad1c9ea590d46efcd1003a44fe157b78491
Tobias <githubst@gmail.com>
2019-06-06 10:49:16 +0200

    ngSwitchCase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'Sullivan';
+  title: string = "Sullivan";
+
+  values: string[] = ["app-intro", "app-bindings", "app-update"];
 }
